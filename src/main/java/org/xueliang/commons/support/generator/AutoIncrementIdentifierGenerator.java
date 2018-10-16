6f1c0e27f42986b73f311a4a90867c36b21f26a7
liangzai_cool <liangzai_cool@yeah.net>
2018-10-16 22:12:32 +0800

    自增序列可以区分appId

--- src/main/java/org/xueliang/commons/support/generator/AutoIncrementIdentifierGenerator.java
+++ src/main/java/org/xueliang/commons/support/generator/AutoIncrementIdentifierGenerator.java
@@ -11,6 +11,12 @@ public class AutoIncrementIdentifierGenerator extends AbstractIdentifierGenerato
     
     private String tableName;
 
+    /**
+     * 自增序列是否区分app_id
+     * 注意：会引起主键冲突，数据库需要使用id和app_id做联合主键
+     */
+    private boolean diffAppId;
+
     @Override
     public String nextId() {
         return nextId(null);
@@ -20,21 +26,45 @@ public class AutoIncrementIdentifierGenerator extends AbstractIdentifierGenerato
     public String nextId(String table) {
         String id = "auto.increment" + (StringUtils.isEmpty(table) ? "" : "." + table);
         long[] value = new long[] {1};
-        generator(id, value);
+        generator(id, "", value);
         return String.valueOf(value[0]);
     }
 
-    protected void generator(String id, long[] value) {
+    /**
+     * 区分appId
+     * 注意：会引起主键冲突，数据库需要使用id和app_id做联合主键
+     * @param table
+     * @param appId
+     * @return
+     */
+    public String nextId(String table, String appId) {
+        String id = "auto.increment" + (StringUtils.isEmpty(table) ? "" : "." + table);
+        long[] value = new long[] {1};
+        String finalAppId = finalAppId(appId);
+        generator(id, finalAppId, value);
+        return String.valueOf(value[0]);
+    }
+
+    protected void generator(String id, String appId, long[] value) {
         dsl.transaction(configuration -> {
-            Record record = dsl.resultQuery("select * from `" + tableName + "` where id = ? and status >= ?", id, DataStatusEnum.NORMAL.getValue()).fetchOne();
+            Record record = dsl.select().from(tableName)
+                    .where("id = ?", id)
+                    .and("status = ?", DataStatusEnum.NORMAL.name())
+                    .and("app_id = ?", appId).fetchOne();
             if (record == null) {
-                dsl.query("delete from `" + tableName + "` where id = ?", id);
-                dsl.query("insert into `" + tableName + "`(id, value, status, created_time) values(?, ?, ?, now())", id, value[0], DataStatusEnum.NORMAL.getValue()).execute();
+                dsl.query("delete from `" + tableName + "` where id = ? and app_id = ?", id, appId);
+                dsl.query("insert into `" + tableName + "`(id, value, app_id, status, created_time) values(?, ?, ?, ?, now())",
+                        id, value[0], appId, DataStatusEnum.NORMAL.name()).execute();
                 return;
             }
             value[0] = record.get("value", Long.class);
-            dsl.query("update `" + tableName + "` set value = ?, updated_time = now() where id = ?", value[0] + 1, id).execute();
+            long nextId = value[0] + 1;
+            dsl.query("update `" + tableName + "` set value = ?, updated_time = now() where id = ? and app_id = ?", nextId, id, appId).execute();
         });
+    }
+
+    private String finalAppId(String appId) {
+        return diffAppId ? appId : StringUtils.EMPTY;
     }
 
     public DSLContext getDsl() {
@@ -49,5 +79,13 @@ public class AutoIncrementIdentifierGenerator extends AbstractIdentifierGenerato
     }
     public void setTableName(String tableName) {
         this.tableName = tableName;
+    }
+
+    public boolean isDiffAppId() {
+        return diffAppId;
+    }
+
+    public void setDiffAppId(boolean diffAppId) {
+        this.diffAppId = diffAppId;
     }
 }
